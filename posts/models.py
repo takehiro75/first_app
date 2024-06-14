81ed85df75010f8d77c016e2b6b3d6e57803c6a5
takehiro75 <takehiro-wada@tokyu-hl.jp>
2024-06-14 12:22:07 +0900

    Initial commit

---
+++ posts/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.utils import timezone 
+
+class Post(models.Model):
+  content = models.TextField(blank=True)
+  created_at = models.DateTimeField(verbose_name='投稿日時', default=timezone.now)
+# Create your models here.
